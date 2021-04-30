988203fc4c901938598278aad2b59e242e90c6d7
Jeany Hsu <g200421@gmail.com>
2021-04-30 16:44:42 +0800

    final version

--- r.py
+++ r.py
@@ -1,14 +1,21 @@
 import random
-
-r = random.randint(1,100)
+start = input('請隨機決定數字範圍開始值: ')
+end = input('請隨機決定數字範圍結束值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
+i = 0
 while True:
 	num = input('請猜個數字: ')
 	num = int(num)
+	i = i + 1
 	if num == r:
 		print('恭喜您猜對')
+		print('你總共猜了',i,'次')
 		break
 	else:
 		if num > r:
 			print('要小於這個數字')
 		elif num < r:
 			print('要大於這個數字唷')
+	print('這是你猜的第',i,'次')
